573b6a2c84cdc7cfdb83c5424319426a4a174377
Javier Algaba <javialgaba@gmail.com>
2026-05-22 17:33:04 +0200

    Add Spector class for canvas capture and update Vite config to define globalThis

--- src/shims/phaser3spectorjs.ts
+++ src/shims/phaser3spectorjs.ts
@@ -1,3 +1,36 @@
-const phaser3spectorjs = {};
+class NoopSpectorCaptureSignal {
+	add() {}
+}
 
+class Spector {
+	onCapture = new NoopSpectorCaptureSignal();
+
+	captureCanvas() {}
+
+	captureNextFrame() {}
+
+	getFps() {
+		return 0;
+	}
+
+	log(message: string) {
+		return message;
+	}
+
+	startCapture() {}
+
+	stopCapture() {
+		return null;
+	}
+
+	getResultUI() {
+		return {
+			display() {},
+		};
+	}
+}
+
+const phaser3spectorjs = { Spector };
+
+export { Spector };
 export default phaser3spectorjs;
